3076ae392894c30946b8b30a47a1dd3a3ae66074
befrancax <befranca82@gmail.com>
2025-03-12 20:36:51 -0300

    operadores2

---
+++ tipos-variaveis/src/Operadores2.java
@@ -0,0 +1,15 @@
+public class Operadores2 {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 6;
+        b = 6;
+        
+        // operador ternário
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+        System.out.println(resultado);
+    }        
+}
+
+
