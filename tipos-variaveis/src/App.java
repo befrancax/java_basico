0a89fd00633ad7597a6ce47afa033687e3f8f7f4
befrancax <befranca82@gmail.com>
2025-03-11 21:04:05 -0300

    PRIMEIRO

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2.500;
+
+        short numeroCurto =1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+
+        // expressão final 'bloqueia' o valor da variavel
+        // e impede que ela seja alterada.
+
+        //ex
+        final double valor_PI = 3.14;
+
+        // OPERADORES
+
+        // ***ATRIBUIÇÃO "="
+        
+        // ARITMÉTICOS "+ - * / %(MÓDULO)"
+
+
+    }
+}
